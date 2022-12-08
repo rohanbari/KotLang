dff2c1ac5820a9c396a390572e43a4df6b858471
rohanbari <rohanbari4@gmail.com>
2022-12-08 12:01:04 +0530

    Read user input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,18 @@
  * The main method accepting [args].
  */
 fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Program arguments: ${args.joinToString()}")
+    if (args.isNotEmpty()) {
+        println("Your name is: ${args.joinToString()}")
+        return
+    }
+
+    print("Enter your name: ")
+    val name = readlnOrNull()
+
+    if (name == null) {
+        println("error: Name must not be null.")
+        return
+    }
+
+    println("Welcome, $name")
 }
