e84650117a35e69aac1dd80cde8e2de13b23d462
rohanbari <rohanbari4@gmail.com>
2022-12-13 10:06:17 +0530

    Custom accessors

---
+++ src/main/Basics/CustomAccessor.kt
@@ -0,0 +1,19 @@
+data class Polygon(val height: Int, val width: Int) {
+    /* Custom accessors (getter) which is not a field. */
+    val isSquare
+        get() = height == width
+
+    val isRectangle
+        get() = height != width
+}
+
+fun main() {
+    // Instantiation gone easy.
+    val model1 = Polygon(10, 15)
+    val model2 = Polygon(10, 10)
+
+    println(
+        "sq => model1: ${model1.isSquare}, model2: ${model2.isSquare}\n" +
+                "rc => model1: ${model1.isRectangle}, model2: ${model2.isRectangle}"
+    )
+}
